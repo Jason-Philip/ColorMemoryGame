67c6f5b7424540ae9cc460d088fc5016caa57df1
Jason-Philip <jasonphilipmoore@outlook.com>
2021-09-10 01:01:48 +0000

    Creating empty functions for operation of game

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+
+/* Triggered when start in clicked allowing the game to begin*/
+function startGame() {
+
+}
+/* Creates random pattern and records it to dictionary*/
+function CreatePattern() {
+    
+}
+/*Pattern occurs with random colors in random sequence*/
+function triggerPattern() {
+
+}
+/*Occurs when user clicks a square triggerring a highlight of the selected color*/
+function flashSquare() {
+
+}
+/* Compares pattern enterred so far with produced pattern*/
+function checkRight() {
+
+}
+/* Triggered when the enterred color/pattern is incorrect and offer try again*/
+function WrongEntry() {
+
+}
+/* Triggered when the enterred color/pattern is correct and offer try again*/
+function RightEntry() {
+    
+}
+
